b29fed40ac0353163e83d9e3130abefdc6a5b51f
tangshiran <tangshiran2022@163.com>
2026-06-28 18:15:21 +0800

    chore: fill install script with download commands

--- installplaserve.sh
+++ installplaserve.sh
@@ -1,3 +1,4 @@
+#依赖准备
 useradd -m steam
 cd /home/steam
 sudo add-apt-repository multiverse
@@ -6,3 +7,10 @@ sudo apt update
 sudo apt install lib32gcc-s1 steamcmd
 ln -s /usr/games/steamcmd steamcmd
 ./steamcmd +login anonymous +app_update 2394010 validate +quit
+
+#拉取脚本和程序
+mkdir -p /opt/palworld-manager/logs
+curl -fsSL https://raw.githubusercontent.com/TTTTSR/PalServeManager/main/palservemanage.sh -o /opt/palworld-manager/palservemanage.sh
+curl -fsSL https://raw.githubusercontent.com/TTTTSR/PalServeManager/main/palworld-manager-linux -o /opt/palworld-manager/palworld-manager-linux
+chmod +x /opt/palworld-manager/palservemanage.sh /opt/palworld-manager/palworld-manager-linux
+chown -R steam:steam /opt/palworld-manager
